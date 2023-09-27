ec705dd21637f2656deb52ba6699220d65a145bc
Ridhiksha <144768030+Ridhiksha@users.noreply.github.com>
2023-09-27 20:48:07 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-def Hello_world():
-    print("Hello world")
+class Player:
+  def Play(self):
+      print("The Player is Playing cricket.")
+    
+class Batsman(Player):
+    def Play(self):
+        print("The batsman is       batting.")
+        
+class Bowler(Player):
+  
+    def Play(self):
+        print("The bowler is bowling.")
+                 
+batsman = Batsman()
+bowler = Bowler()
 
-Hello_world()
+
+batsman.Play()
+bowler.Play()
